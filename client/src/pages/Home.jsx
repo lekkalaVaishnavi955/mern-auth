81863d66d89fff691ed9bd4ead854451ac806650
Vaishnavi Lekkala <vyshnavireddy955@gmail.com>
2025-06-05 12:36:15 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
+import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 
 function Home() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
-        <NavBar/>
+        <Navbar/>
         <Header/>
     </div>
 
