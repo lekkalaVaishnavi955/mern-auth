b7be2133993f9fc83ddaf4342877edd4cdf8e56a
Vaishnavi Lekkala <vyshnavireddy955@gmail.com>
2025-06-05 13:59:35 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -56,7 +56,7 @@ export const login = async (req , res)=>{
         }
         const token = jwt.sign({id:user._id},process.env.JWT_SECRET , {expiresIn:'7d'});
         res.cookie('token' , token ,{
-            httpOnly:true,secure:ture ,
+            httpOnly:true,secure:true ,
             sameSite:"None",
             maxAge:7*24*60*60*1000
         });
