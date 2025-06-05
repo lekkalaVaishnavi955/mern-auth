ebcf008eefac0bc4b2349cd5fd87f7a402b9a0ca
Vaishnavi Lekkala <vyshnavireddy955@gmail.com>
2025-06-05 13:57:25 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -70,8 +70,8 @@ export const login = async (req , res)=>{
 export const logout = async (req , res)=>{
     try{
         res.clearCookie('token' ,{
-            httpOnly:true,secure:process.env.NODE_ENV === 'production' ,
-            sameSite:process.env.NODE_ENV==='production'?'none':'strict'
+            httpOnly:true,secure:true ,
+            sameSite:"None"
         });
          return res.json({success:true , message:'logged out'});
     }catch(error){
